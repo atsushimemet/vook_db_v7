9af8a3c2655b9fdaaa6807e4212c685d4233376b
atsushimemet <noap3b69n@gmail.com>
2023-09-02 17:55:31 +0900

    update: new repo get_rakuten modules runned

--- vook_db_v7/config.py
+++ vook_db_v7/config.py
@@ -1,6 +1,7 @@
 import datetime
-from vook_db_v7.local_config import ClientId, sid, pid  # noqa
+from vook_db_v7.local_config import ClientId, sid, pid, CLIENT_ME  # noqa
 
+# Yahoo
 REQ_URL_CATE = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
 PLATFORM_ID = 2
 BRAND = "リーバイス"
@@ -36,3 +37,26 @@ run_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 start_num = 1
 step = 100
 max_products = 1000
+
+# Rakuten
+REQ_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+MAX_PAGE = 10  # NOTE: auto_postが300ほどしかできない。
+HITS_PER_PAGE = 30
+
+
+req_params = {
+    "applicationId": CLIENT_ME["APPLICATION_ID"],
+    "affiliateId": CLIENT_ME["AFF_ID"],
+    "format": "json",
+    "formatVersion": "2",
+    "keyword": "",
+    "hits": HITS_PER_PAGE,
+    "sort": "-itemPrice",
+    "page": 0,
+    "minPrice": 100,
+}
+WANT_ITEMS = [
+    "itemName",
+    "itemPrice",
+    "itemUrl",
+]
