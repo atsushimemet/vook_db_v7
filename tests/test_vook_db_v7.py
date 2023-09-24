3d98137d148e0b6b448231ab80a0ba7b5941ee4a
atsushimemet <noap3b69n@gmail.com>
2023-09-25 01:23:54 +0900

    close #1 (#2)
    * update: new repo get_rakuten modules runned
    
    * update: unittest by Koga
    
    * update: unnecessary true desc
    
    * add: src from Koga
    
    * test: passes unittests
    
    * refactor: unittest

--- tests/test_vook_db_v7.py
+++ tests/test_vook_db_v7.py
@@ -1,5 +1,72 @@
-from vook_db_v7 import __version__
+"""仕様書: https://docs.google.com/spreadsheets/d/1KK8aWAntbFzdJ-_642IKRACOqINbjdgnj68Ijwkry-U/edit#gid=0"""
+from vook_db_v7.rakuten_api_call import output
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+columns_correct = [
+    "id",
+    "name",
+    "url",
+    "price",
+    "knowledge_id",
+    "pltaform_id",
+    "size_id",
+    "created_at",
+    "updated_at",
+]
+
+
+def test_columns_names_checker():
+    file = output(
+        brand="リーバイス levis", item="501 66前期", platform_id=1, item_id=1, knowledge_id=1
+    )
+    if all(file.columns == columns_correct):
+        print("columns ok!")
+    else:
+        print("incorrect columns!")
+
+
+class TestColumnTypeChecker:
+    file = output(
+        brand="リーバイス levis",
+        item="501 66前期",
+        platform_id=1,
+        item_id=1,
+        knowledge_id=1,
+    )
+
+    def test_id_checker(self):
+        if self.file[columns_correct[0]].notnull().all():
+            assert self.file[columns_correct[0]].dtypes == "int64"
+
+    def test_name_checker(self):
+        if self.file[columns_correct[1]].notnull().all():
+            assert self.file[columns_correct[1]].dtypes == "O"
+
+    def test_url_checker(self):
+        if self.file[columns_correct[2]].notnull().all():
+            assert self.file[columns_correct[2]].dtypes == "O"
+
+    def test_price_checker(self):
+        if self.file[columns_correct[3]].notnull().all():
+            assert self.file[columns_correct[3]].dtypes == "int64"
+
+    def test_knowledge_id_checker(self):
+        if self.file[columns_correct[4]].notnull().all():
+            assert self.file[columns_correct[4]].dtypes == "int64"
+
+    def test_pltaform_id_checker(self):
+        if self.file[columns_correct[5]].notnull().all():
+            assert self.file[columns_correct[5]].dtypes == "int64"
+
+    def test_size_id_checker(self):
+        """Tolerate defects. When size_id is not null, O2int64"""
+        if self.file[columns_correct[6]].notnull().all():
+            assert self.file[columns_correct[6]].dtypes == "O"
+
+    def test_created_at_checker(self):
+        if self.file[columns_correct[7]].notnull().all():
+            assert self.file[columns_correct[7]].dtypes == "O"
+
+    def test_updated_at_checker(self):
+        if self.file[columns_correct[8]].notnull().all():
+            assert self.file[columns_correct[8]].dtypes == "O"
